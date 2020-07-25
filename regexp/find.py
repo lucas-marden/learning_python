c404ae83a4780c007d5645686b015031e34940fc
Lucas Marden <lucas@marden.org>
2020-07-25 19:57:24 -0400

    Support multiple instances of each search and multiple searches

--- regexp/find.py
+++ regexp/find.py
@@ -8,34 +8,47 @@ import sys
 def find(input, text):
 	p = re.compile('(.*?)({})(.*)'.format(input), re.I | re.DOTALL)
 	matches = p.search(text)
-	return matches
+	if matches:
+		return matches
+	else:
+		return None
 
-def recombine(matches):
+def recombine(matches, seen):
 	first_section = matches.group(1)
 	key_word = matches.group(2)
 	closing_section = matches.group(3)	
-	one = '\033[0;37;40m{}'.format(first_section)
-	two = '\033[0;30;47m{}'.format(key_word)
-	three = '\033[0;37;40m{}'.format(closing_section)
-	x = '{}{}{}'.format(one,two,three)
+	one = '\033[0m{}'.format(first_section)
+	two = '\033[33m{}'.format(key_word)
+	three = '\033[0m{}'.format(closing_section)
+	x = '{}{}{}{}'.format(seen,one,two,three)
 	return x
 
-def step(matches):
-	seen = matches.group(1) + matches.group(2)
+def step(matches, previous):
+	seen = previous + matches.group(1) + matches.group(2)
 	next = matches.group(3)
 	return seen, next;
 
 for x in sys.argv[1:]:
-	f = open('speech.txt', 'r')
-	text = f.read()
-
-	print('\033[0;47;40m{}'.format(text))
-	print('Search:', end='')
-	input = input()
-	print(recombine(find(input,text)))
-	seen, next = step(find(input, text))
+	i = 0
+	while i<1000:
+		f = open(x, 'r')
+		text = f.read()
+		print('\033[0m{}'.format(text))
+		print('\033[0mSearch:', end='')
+		key = input()
+		seen = ''
+		a = 0
+		while a<len(text):
+			if find(key, text):
+				print(recombine(find(key,text), seen))
+				seen, next = step(find(key, text), seen)
+				if input() == '':
+					text = next
+					a += 1
+			else:
+				break
+		i += 1
 	f.close()
-
 
 
 
